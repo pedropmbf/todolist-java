e6633fd22e3a197fd6cedde36bfb3f7e9e44298f
Pedro Paulo Bezerra <pedropmbf@outlook.com>
2024-09-10 12:56:56 -0300

    Service layer, component registration

--- src/main/java/com/pedropmbf/todolistjava/resources/TaskResource.java
+++ src/main/java/com/pedropmbf/todolistjava/resources/TaskResource.java
@@ -1,18 +1,33 @@
 package com.pedropmbf.todolistjava.resources;
 
 import com.pedropmbf.todolistjava.entities.Task;
+import com.pedropmbf.todolistjava.services.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/tasks")
 public class TaskResource {
 
+    @Autowired
+    private TaskService service;
+
     @GetMapping
-    public ResponseEntity<Task> findAll() {
-        Task task = new Task(1L, "Teste", "task de teste", "Ok");
-        return ResponseEntity.ok().body(task);
+    public ResponseEntity<List<Task>> findAll() {
+        List<Task> list = service.findAll();
+        return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Task> findById(@PathVariable Long id) {
+        Task obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
 }
---
+++ src/main/java/com/pedropmbf/todolistjava/services/TaskService.java
@@ -0,0 +1,25 @@
+package com.pedropmbf.todolistjava.services;
+
+import com.pedropmbf.todolistjava.entities.Task;
+import com.pedropmbf.todolistjava.repositories.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository repository;
+
+    public List<Task> findAll() {
+        return repository.findAll();
+    }
+
+    public Task findById(Long id) {
+        Optional<Task> obj = Optional.of(repository.findById(id).get());
+        return obj.get();
+    }
+}
