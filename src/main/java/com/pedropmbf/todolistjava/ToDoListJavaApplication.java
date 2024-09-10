4410ed7353681a6c2c1e400ec8c024f8a6fc55d7
Pedro Paulo Bezerra <pedropmbf@outlook.com>
2024-09-10 10:29:12 -0300

    Adding project to github

---
+++ src/main/java/com/pedropmbf/todolistjava/ToDoListJavaApplication.java
@@ -0,0 +1,13 @@
+package com.pedropmbf.todolistjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoListJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pedropmbf/todolistjava/ToDoListJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedropmbf.todolistjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
