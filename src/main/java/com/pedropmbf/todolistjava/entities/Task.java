9271c4995cd751e4942628f53d406b1d9ba4fcef
Pedro Paulo Bezerra <pedropmbf@outlook.com>
2024-09-10 11:22:35 -0300

    Added H2 and JPA database

--- src/main/java/com/pedropmbf/todolistjava/entities/Task.java
+++ src/main/java/com/pedropmbf/todolistjava/entities/Task.java
@@ -1,11 +1,19 @@
 package com.pedropmbf.todolistjava.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Task implements Serializable {
 
     public static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String title;
     private String description;
